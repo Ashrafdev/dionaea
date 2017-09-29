313583c3abbec7400f11f32bb01539ccafb85eea
Fatah al Farihin <30997999+cikgufatah@users.noreply.github.com>
2017-09-29 12:04:29 +0800

    Lebahnet Initial

--- modules/python/scripts/upnp/upnp.py
+++ modules/python/scripts/upnp/upnp.py
@@ -43,167 +43,172 @@ logger.setLevel(logging.DEBUG)
 httpversion = ''
 
 class upnpreq:
-	def __init__(self, header):
-		hlines = header.split(b'\n')
-		req = hlines[0]
-		reqparts = req.split(b" ")
-		self.type = reqparts[0]
-		self.path = urllib.parse.unquote(reqparts[1].decode('utf-8'))
-		global httpversion
-		httpversion = reqparts[2]
-		r = httpversion.find(b"\r")
-		if r:
-			httpversion = httpversion[:r]
-		self.headers = {}
-		for hline in hlines[1:]:
-			if hline[len(hline)-1] == 13: # \r
-				hline = hline[:len(hline)-1]
-			hset = hline.split(b":", 1)
-			try:
-				self.headers[hset[0].lower()] = hset[1].strip()
-			except:
-				logger.info('potential upnp exploit: {0}'.format(hset[0]))
-		
-	def print(self):
-		logger.debug(self.type + b" " + self.path.encode('utf-8') + b" " + httpversion)
-		for i in self.headers:
-			logger.debug(i + b":" + self.headers[i])
-
+    def __init__(self, header):
+        hlines = header.split(b'\n')
+        req = hlines[0]
+        reqparts = req.split(b" ")
+        self.type = reqparts[0]
+        self.path = urllib.parse.unquote(reqparts[1].decode('utf-8'))
+        global httpversion
+        httpversion = reqparts[2]
+        r = httpversion.find(b"\r")
+        if r:
+            httpversion = httpversion[:r]
+        self.headers = {}
+        for hline in hlines[1:]:
+            if hline[len(hline)-1] == 13: # \r
+                hline = hline[:len(hline)-1]
+            hset = hline.split(b":", 1)
+            try:
+                self.headers[hset[0].lower()] = hset[1].strip()
+            except:
+                logger.info('potential upnp exploit: {0}'.format(hset[0]))
+        
+    def print(self):
+        logger.debug(self.type + b" " + self.path.encode('utf-8') + b" " + httpversion)
+        for i in self.headers:
+            logger.debug(i + b":" + self.headers[i])
 
 class upnpd(connection):
-	def __init__(self, proto='udp'):
-		connection.__init__(self,proto)
-		self.state = 'HEADER'
-		self.rwchunksize = 64*1024
-		self._out.speed.limit = 16*1024
-		self.env = None
-		self.boundary = None
-		self.fp_tmp = None
-		self.cur_length = 0
-		max_request_size = 32768
-		self.personalities = ''
-		self.loaded = ''
+    def __init__(self, proto='udp'):
+        connection.__init__(self,proto)
+        self.state = 'HEADER'
+        self.rwchunksize = 64*1024
+        self._out.speed.limit = 16*1024
+        self.env = None
+        self.boundary = None
+        self.fp_tmp = None
+        self.cur_length = 0
+        max_request_size = 32768
+        self.personalities = ''
+        self.loaded = ''
 
-		try:
-			if 'max-request-size' in g_dionaea.config()['modules']['python']['upnp']:
-				# try to convert value to int
-				max_request_size = int(g_dionaea.config()['modules']['python']['upnp']['max-request-size'])
-			else:
-				logger.info("Value for 'max-request-size' not found, using default value.")
-		except:
-			logger.warning("Error while converting 'max-request-size' to an integer value. Using default value.")
+        try:
+            if 'max-request-size' in g_dionaea.config()['modules']['python']['upnp']:
+                # try to convert value to int
+                max_request_size = int(g_dionaea.config()['modules']['python']['upnp']['max-request-size'])
+            else:
+                logger.info("Value for 'max-request-size' not found, using default value.")
+        except:
+            logger.warning("Error while converting 'max-request-size' to an integer value. Using default value.")
 
-		self.max_request_size = max_request_size * 1024
+        self.max_request_size = max_request_size * 1024
 
-		# load the UPnP device personalities from dionaea.conf file 
-		try:
-			if 'personalities-enable' in g_dionaea.config()['modules']['python']['upnp']:
-				loaded = g_dionaea.config()['modules']['python']['upnp']['personalities-enable']
-				self.personalities = g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['cache']
-				self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['st']
-				self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['usn']
-				self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['server']
-				self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['location']
-				self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['opt']
+        # load the UPnP device personalities from dionaea.conf file 
+        try:
+            if 'personalities-enable' in g_dionaea.config()['modules']['python']['upnp']:
+                loaded = g_dionaea.config()['modules']['python']['upnp']['personalities-enable']
+                self.personalities = g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['cache']
+                self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['st']
+                self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['usn']
+                self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['server']
+                self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['location']
+                self.personalities += g_dionaea.config()['modules']['python']['upnp']['personalities'][loaded]['opt']
 
-				logger.info("loading emulated UPnP device with personalities: '" + loaded + "'")
-			else:
-				logger.info("Value for 'personalities' not found, using default value.")
-		except:
-			logger.warning("Error while retrieve 'personalities'. Using default value.")
-
-
-
-	def handle_origin(self, parent):
-		#pass
-		self.root = parent.root
-		self.rwchunksize = parent.rwchunksize
-
-	def handle_established(self):
-		logger.debug("{:s} handle_established".format(self))
-
-		self.timeouts.idle = 10
-		self.timeouts.sustain = 120
-		self.processors()
-
-		# fake a connection entry
-		i = incident("dionaea.connection.udp.connect")
-		i.con = self
-		i.report()
-
-	def chroot(self, path):
-		self.root = path
-
-	def handle_io_in(self, data):
-		if self.state == 'HEADER':
-			# End Of Head
-			eoh = data.find(b'\r\n\r\n')
-			# Start Of Content
-			soc = eoh + 4
-
-			if eoh == -1:
-				eoh = data.find(b'\n\n')
-				soc = eoh + 2
-			if eoh == -1:
-				return 0
-
-			header = data[0:eoh]
-			data = data[soc:]
-			self.header = upnpreq(header)
-
-			self.header.print()
-
-			# current Dionaea supports header type 'M-SEARCH' for SSDP discovery
-			if self.header.type == b'M-SEARCH':
-				self.handle_MSEARCH()
-				return len(data)
-
-			# header not found
-			else:
-				logger.info('unknown upnp ssdp header: {0}'.format(self.header.type))
-				self.handle_unknown()
-			return len(data)
-
-		else:
-			self.close()
-			return len(data)
+                logger.info("loading emulated UPnP device with personalities: '" + loaded + "'")
+            else:
+                logger.info("Value for 'personalities' not found, using default value.")
+        except:
+            logger.warning("Error while retrieve 'personalities'. Using default value.")
 
 
-	def handle_MSEARCH(self):
-		"""Handle the M-SEARCH method"""
-		r = self.personalities
-		self.send_response_upnp(200, r)
-		self.close()
 
-	def handle_unknown(self):
-		pass
+    def handle_origin(self, parent):
+        #pass
+        self.root = parent.root
+        self.rwchunksize = parent.rwchunksize
 
-	def handle_io_out(self):
-		logger.debug("handle_io_out")
+    def handle_established(self):
+        logger.debug("%r handle_established", self)
+
+        self.timeouts.idle = 10
+        self.timeouts.sustain = 120
+        self.processors()
+
+        # fake a connection entry
+        i = incident("dionaea.connection.udp.connect")
+        i.con = self
+        i.report()
+
+    def chroot(self, path):
+        self.root = path
+
+    def handle_io_in(self, data):
+        if self.state == 'HEADER':
+            # End Of Head
+            eoh = data.find(b'\r\n\r\n')
+            # Start Of Content
+            soc = eoh + 4
+
+            if eoh == -1:
+                eoh = data.find(b'\n\n')
+                soc = eoh + 2
+            if eoh == -1:
+                return 0
+
+            header = data[0:eoh]
+            data = data[soc:]
+            self.header = upnpreq(header)
+
+            i = incident("dionaea.modules.python.upnp.request")
+            i.con = self
+            i.headers = str(header)
+            i.data = str(data)
+            i.report()
+
+            self.header.print()
+
+            # current Dionaea supports header type 'M-SEARCH' for SSDP discovery
+            if self.header.type == b'M-SEARCH':
+                self.handle_MSEARCH()
+                return len(data)
+
+            # header not found
+            else:
+                logger.info('unknown upnp ssdp header: {0}'.format(self.header.type))
+                self.handle_unknown()
+            return len(data)
+
+        else:
+            self.close()
+            return len(data)
 
 
-	def send_response_upnp(self, code, upnp_response):
-		if code in self.responses:
-			message = self.responses[code][0]
-		else:
-			message = ''
-		self.version  = httpversion[-3:].decode('utf-8')
-		self.send("%s/%s %d %s\r\n%s\r\n" % ("HTTP", self.version, code, message, upnp_response))
+    def handle_MSEARCH(self):
+        """Handle the M-SEARCH method"""
+        r = self.personalities
+        self.send_response_upnp(200, r)
+        self.close()
 
-	def handle_timeout_sustain(self):
-		logger.debug("{:s} handle_timeout_sustain".format(self))
-		return True
+    def handle_unknown(self):
+        pass
 
-	def handle_timeout_idle(self):
-		logger.debug("{:s} handle_timeout_idle".format(self))
-		self.close()
-		return False
-
-	def handle_disconnect(self):
-		return False
+    def handle_io_out(self):
+        logger.debug("handle_io_out")
 
 
-	responses = {
-		200: ('OK', 'Request fulfilled, document follows'),
-		}
+    def send_response_upnp(self, code, upnp_response):
+        if code in self.responses:
+            message = self.responses[code][0]
+        else:
+            message = ''
+        self.version  = httpversion[-3:].decode('utf-8')
+        self.send("%s/%s %d %s\r\n%s\r\n" % ("HTTP", self.version, code, message, upnp_response))
+
+    def handle_timeout_sustain(self):
+        logger.debug("%r handle_timeout_sustain", self)
+        return True
+
+    def handle_timeout_idle(self):
+        logger.debug("%r handle_timeout_idle", self)
+        self.close()
+        return False
+
+    def handle_disconnect(self):
+        return False
+
+
+    responses = {
+        200: ('OK', 'Request fulfilled, document follows'),
+        }
 
